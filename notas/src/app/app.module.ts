79eef790cad0ac497d7e79c93a93f6d23485dd43
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-01 22:47:12 -0500

    add firebase configurations

--- notas/src/app/app.module.ts
+++ notas/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"notas-f03cc","appId":"1:1022602080652:web:1cb1a1741cdec37f08a6a9","storageBucket":"notas-f03cc.appspot.com","apiKey":"AIzaSyAJ9gWBnzVmj1V_SyxH_P3jtiZR9JSnIXA","authDomain":"notas-f03cc.firebaseapp.com","messagingSenderId":"1022602080652"})),
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
