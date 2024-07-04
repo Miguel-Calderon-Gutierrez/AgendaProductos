5c3ff6868ba08eba5e03e640123160388188cfe1
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-03 20:11:51 -0500

    cambios en gitginore

--- notas/src/environments/environment.development.ts
+++ notas/src/environments/environment.development.ts
@@ -2,4 +2,3 @@ export const environment = {
   firebase :
   {"projectId":"notas-f03cc","appId":"1:1022602080652:web:1cb1a1741cdec37f08a6a9","storageBucket":"notas-f03cc.appspot.com","apiKey":"AIzaSyAJ9gWBnzVmj1V_SyxH_P3jtiZR9JSnIXA","authDomain":"notas-f03cc.firebaseapp.com","messagingSenderId":"1022602080652"}
   };
-
--- notas/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-firebase :
-{"projectId":"notas-f03cc","appId":"1:1022602080652:web:1cb1a1741cdec37f08a6a9","storageBucket":"notas-f03cc.appspot.com","apiKey":"AIzaSyAJ9gWBnzVmj1V_SyxH_P3jtiZR9JSnIXA","authDomain":"notas-f03cc.firebaseapp.com","messagingSenderId":"1022602080652"}
-};
